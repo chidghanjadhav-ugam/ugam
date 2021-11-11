a80c9a1061f36914aba2ef079d684c1aa6c85ca0
Chidghan Jadhav <chidghan.jadhav@ugamsolutions.com>
2021-11-11 13:44:38 +0530

    Jackson Commit

--- core/src/main/java/com/ugam/core/models/impl/TestimonialImpl.java
+++ core/src/main/java/com/ugam/core/models/impl/TestimonialImpl.java
@@ -1,17 +1,27 @@
 package com.ugam.core.models.impl;
 
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
 import com.ugam.core.models.Testimonial;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Exporter;
+import org.apache.sling.models.annotations.ExporterOption;
 import org.apache.sling.models.annotations.Model;
 
 import javax.inject.Inject;
 
 @Model(adaptables = Resource.class,
         adapters = Testimonial.class,
+        resourceType = "ugam/components/content/testimonial",
         defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
 )
+@Exporter(name="jackson", extensions = "json",
+        options = {
+                @ExporterOption(name = "SerializationFeature.WRAP_ROOT_VALUE", value = "true")
+        })
+@JsonRootName("Testimonial Exporter")
 public class TestimonialImpl implements Testimonial{
 
     @Inject
@@ -36,6 +46,11 @@ public class TestimonialImpl implements Testimonial{
     @Override
     public String getTitle() {
         return title;
+    }
+
+    @JsonProperty(value = "custom property")
+    public String testimonial(){
+        return "Testimonial";
     }
 
 }
