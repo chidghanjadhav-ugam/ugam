084bedd53a460b3d04c96f6f3db0daa649ccf14a
Chidghan Jadhav <chidghan.jadhav@ugamsolutions.com>
2021-11-19 00:57:47 +0530

    Servlet with Resource

---
+++ core/src/main/java/com/ugam/core/servlets/ResourceType.java
@@ -0,0 +1,41 @@
+package com.ugam.core.servlets;
+
+
+import com.day.cq.wcm.api.Page;
+import com.day.cq.wcm.api.PageManager;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Component(service = Servlet.class)
+@SlingServletResourceTypes(
+        resourceTypes = "ugam/components/page"
+)
+public class ResourceType extends SlingSafeMethodsServlet {
+
+
+    protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws IOException{
+
+        final ResourceResolver resourceResolver = request.getResourceResolver();
+        Page page = resourceResolver.adaptTo(PageManager.class).getPage("/content/ugam/us/en");
+        List pageList = new ArrayList();
+        Iterator<Page> childPages = page.listChildren();
+        while (childPages.hasNext()){
+            Page childPage =childPages.next();
+            String pageName;
+            pageName = childPage.getTitle();
+            pageList.add(pageName);
+        }
+        response.setContentType("text/html");
+        response.getWriter().print(pageList);
+    }
+}
